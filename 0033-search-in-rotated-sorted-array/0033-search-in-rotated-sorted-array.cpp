0e8d26cc5b3de679fd9d6db2fb038e7d8746675b
Karanjit08 <121970119+Karanjit08@users.noreply.github.com>
2023-01-21 11:03:21 +0530

    Time: 5 ms (59.63%), Space: 11 MB (97.34%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int findPivot(vector<int>& nums)
     {
+        int left=0,right,mid,ans;
         int n=nums.size();
-        int left=0,right=n-1,mid;
+        right = n-1;
         while(left<right)
         {
-            mid = left + (right-left)/2;
+            mid = left+(right-left)/2;
             if(nums[mid]>=nums[0])
             {
                 left = mid+1;
@@ -18,15 +19,12 @@ public:
         }
         return left;
     }
-    
-    int b1(vector<int>& nums, int target, int left, int right)
+    int binarySearch(vector<int>& nums, int target, int left,int right)
     {
-        int n=nums.size();
-        
         int mid;
         while(left<=right)
         {
-            mid = left+(right-left)/2;
+            mid = left + (right-left)/2;
             if(nums[mid]==target)
             {
                 return mid;
@@ -37,30 +35,24 @@ public:
             }
             else
             {
-                right =mid-1;
+                right = mid-1;
             }
         }
         return -1;
     }
     
     int search(vector<int>& nums, int target) {
-        int n=nums.size();
+        int n=nums.size(),ans;
+        int left=0,right=n-1,mid;
         int pivot = findPivot(nums);
-        int secondPivot;
-        secondPivot= pivot - 1;
-        int ans;
-
         if(nums[pivot]<=target && nums[n-1]>=target)
         {
-            
-            ans = b1(nums,target,pivot,n-1);
-               
+            ans = binarySearch(nums,target,pivot,n-1);
         }
         else
         {
-            ans = b1(nums,target,0,secondPivot);
+            ans = binarySearch(nums,target,0,pivot-1);
         }
-        
         return ans;
         
         
