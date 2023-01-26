c2be26b132d997478fa087868356e08a6310b80e
Karanjit08 <121970119+Karanjit08@users.noreply.github.com>
2023-01-26 18:15:40 +0530

    Time: 3 ms (89.06%), Space: 9.3 MB (96.69%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,17 +4,26 @@ public:
         int i,j,m,n;
         n = matrix.size();
         m = matrix[0].size();
-        for(i=0;i<n;i++)
+        int left=0,right=m*n-1;
+        int mid;
+        while(left<=right)
         {
-            for(j=0;j<m;j++)
+            mid = left+(right-left)/2;
+            if(matrix[mid/m][mid%m]==target)
             {
-                if(matrix[i][j]==target)
-                {
-                    return true;
-                }
+                return true;
+            }
+            else if(matrix[mid/m][mid%m]<target)
+            {
+                left = mid+1;
+            }
+            else
+            {
+                right = mid-1;
             }
         }
         return false;
+        
         
     }
 };
