8fd127c2c3b3f71d7cabc5fce1928633c4c5e4b4
Karanjit08 <121970119+Karanjit08@users.noreply.github.com>
2023-04-24 21:48:33 +0530

    Time: 37 ms (25.92%), Space: 25 MB (62.24%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,30 +2,41 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
-        int i;
+        int left=0,right=n-1;
         k = k%n;
-        int left=0,right=n-k-1;
-        int left2 = n-k,right2=n-1;
-        while(left<=right)
+        if(k>n)
         {
-            swap(nums[left],nums[right]);
-            left++;
-            right--;
+            int l=0,r=n-1;
+            while(l<=r)
+            {
+                swap(nums[l],nums[r]);
+                l++;
+                r--;
+            }
         }
-        while(left2<=right2)
+        else
         {
-            swap(nums[left2],nums[right2]);
-            left2++;
-            right2--;
-        }
-        int s=0,e=n-1;
-        while(s<e)
-        {
-            swap(nums[s],nums[e]);
-            s++;
-            e--;
+            while(left<=right)
+            {
+                swap(nums[left],nums[right]);
+                left++;
+                right--;
+            } 
+            int left2=0,right2=k-1;
+            while(left2<=right2)
+            {
+                swap(nums[left2],nums[right2]);
+                left2++;
+                right2--;
+            }
+            int left3=k,right3=n-1;
+            while(left3<=right3)
+            {
+                swap(nums[left3],nums[right3]);
+                left3++;
+                right3--;
+            }
         }
         
-           
     }
 };
