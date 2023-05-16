c0165ef1dc4d332e488c2389f9ed28abd92bcd24
Karanjit08 <121970119+Karanjit08@users.noreply.github.com>
2023-05-16 23:10:04 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (36.53%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.cpp
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -2,33 +2,17 @@ class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
         int i;
-        // for(i=0;i<jewels.length();i++)
-        // {
-        //     if(jewels[i]>=65 && jewels[i]<=90)
-        //     {
-        //         jewels[i] = jewels[i] + 32;
-        //     }
-        // }
-        // for(i=0;i<stones.length();i++)
-        // {
-        //     if(stones[i]>=65 && stones[i]<=90)
-        //     {
-        //         stones[i] = stones[i] + 32;
-        //     }
-        // }
         int index=0;
         int count=0;
-        while(index<jewels.length())
+        map<char,int>m;
+        for(i=0;i<jewels.length();i++)
         {
-            char ch = jewels[index];
-            for(i=0;i<stones.length();i++)
-            {
-                if(ch==stones[i])
-                {
-                    count++;
-                }
-            }
-            index++;
+            m.insert({jewels[i],1});
+        }
+        for(i=0;i<stones.length();i++)
+        {
+            char ch = stones[i];
+            count = count + m.count(ch);
         }
         return count;
         
