edf27c45b162815d9e1af4676e4c6ed408bbca9c
Karanjit08 <121970119+Karanjit08@users.noreply.github.com>
2023-05-22 23:31:39 +0530

    Time: 17 ms (81.35%), Space: 6.9 MB (90.12%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -9,8 +9,8 @@ public:
         for(i=1;i<n;i++)
         {
             //ODD CASE
-            low = i+1;
-            high = i-1;
+            low = i-1;
+            high = i+1;
             while(low>=0 && high<n && s[low]==s[high])
             {
                 if(high-low+1>end)
