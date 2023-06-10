ff55b55bace74c2cc6cbddc400b414d63b05c221
Karanjit08 <121970119+Karanjit08@users.noreply.github.com>
2023-06-10 22:42:57 +0530

    Time: 36 ms (7.87%), Space: 46.3 MB (49.40%) - LeetHub

---
+++ 2717-semi-ordered-permutation/2717-semi-ordered-permutation.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int semiOrderedPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int count=0;
+        int index;
+        int i;
+        
+        if(nums[0]!=1)
+        {
+            for(i=0;i<n;i++)
+            {
+                if(nums[i]==1)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            for(i=index;i>0;i--)
+            {
+                // nums[i] = nums[i-1];
+                swap(nums[i],nums[i-1]);
+                count++;
+            }
+            // nums[0]=1;
+        }
+        
+        if(nums[n-1]!=n)
+        {
+            for(i=0;i<n;i++)
+            {
+                if(nums[i]==n)
+                {
+                    index=i;
+                    break;
+                }
+            }
+            for(i=index;i<n-1;i++)
+            {
+                nums[i] = nums[i+1];
+                count++;
+            }
+            // nums[n-1] = n;
+        }
+        return count;
+    }
+};
