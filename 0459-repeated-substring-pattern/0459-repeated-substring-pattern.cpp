8372402f673325809bc67d84f08123a5983444a3
Karanjit08 <121970119+Karanjit08@users.noreply.github.com>
2023-07-13 14:40:55 +0530

    Time: 44 ms (27.70%), Space: 13.3 MB (72.71%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -2,19 +2,23 @@ class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
         int n = s.size();
-        string s1 = "", s2 = "";
-        for(int i=1; i<=n/2; i++) {
-            if (n%i == 0) {
-                s1 = s.substr(0, i);
-                s2 = "";
-                for(int j=0; j<n/i; j++) {
-                    s2 += s1;
+        int i;
+        string ans="";
+        string k = "";
+        for(i=1;i<=n/2;i++){
+            if(n%i==0){
+                k = s.substr(0,i);
+                ans = "";
+                for(int j=0;j<n/i;j++){
+                    ans+=k;
+                }
+                if(ans==s){
+                    return true;
                 }
                 
-                if(s==s2) return true;
             }
         }
-		return false;
+        return false;
         
     }
 };
