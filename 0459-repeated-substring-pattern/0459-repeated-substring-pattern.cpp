05d6d248e5aa50a787317a62b4835738fee86fe8
Karanjit08 <121970119+Karanjit08@users.noreply.github.com>
2023-08-29 22:58:50 +0530

    Time: 55 ms (36.59%), Space: 91.7 MB (32.27%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
+    // bool checkString(string& temp, string s){
+    //     string t;
+    //     int index=0;
+    //     int n = s.length();
+    //     while(index<=n){
+    //         t = t + temp;
+    //         index = index + temp.length();
+    //     }
+    //     cout<<t;
+    //     if(t==s){
+    //         return true;
+    //     }
+    //     return false;
+    // }
+    
     bool repeatedSubstringPattern(string s) {
-        int n = s.size();
+        int n=s.length();
         int i;
-        string ans="";
-        string k = "";
+        // string temp;
         for(i=1;i<=n/2;i++){
             if(n%i==0){
-                k = s.substr(0,i);
-                ans = "";
-                for(int j=1;j<=n/i;j++){
-                    ans+=k;
+                string temp = s.substr(0,i);
+                string ans="";
+                for(int j=0;j<n/i;j++){
+                    ans += temp;
                 }
+            // cout<<ans;
                 if(ans==s){
                     return true;
                 }
-                
             }
         }
+        // cout<<temp/;
+        // cout<<n;
         return false;
-        
     }
 };
