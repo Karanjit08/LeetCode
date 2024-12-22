eeb24c24ed2665eaa263c2a0eda97447f4c5dc09
Karanjit Singh Randhawa <karanjitrandhawa2000@gmail.com>
2024-12-22 11:36:11 +0530

    Time: 4 ms (60.39%), Space: 51.7 MB (23.80%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -7,13 +7,14 @@ var twoSum = function(nums, target) {
     
     var i,j;
     var ans = [];
+    var obj = new Map();
     for(i=0;i<nums.length;i++){
-        for(j=i+1;j<nums.length;j++){
-            if(nums[i]+nums[j]==target){
-                ans.push(i);
-                ans.push(j);
-            }
+        var val = target - nums[i];
+        if(obj.has(val)){
+             ans.push(i);
+            ans.push(obj.get(val));
         }
+        obj.set(nums[i],i);
     }
     return ans;
 };
